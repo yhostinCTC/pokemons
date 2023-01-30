a07968e9c7406436824a96c5cc7876b4b2f3f028
YhostinCTC <yollachica@crackthecode.la>
2023-01-30 08:43:39 -0500

    feat: border cards

--- script.js
+++ script.js
@@ -14,51 +14,20 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-
 function renderPokeCardData(data){
     let cardPokemon = document.createElement("div");
-    cardPokemon.className = "card_pokemon";
+    cardPokemon.className = `card_pokemon ${data.types[0].type.name}`;
     cardPokemon.innerHTML = `
         <h3 class="card_name">${data.name}</h3>
         <img class="card_image" src="${data.sprites.other["official-artwork"].front_default}">
         <div class="card_information">
             <div class="information_item">
                 <h4 class="information_title">Movements</h4>
-                <div class="container_moves">
-                    <p class="moves_item">${data.moves[getRandomInt(data.moves.length)].move.name}</p>
-                    <p class="moves_item">${data.moves[getRandomInt(data.moves.length)].move.name}</p>
-                    <p class="moves_item">${data.moves[getRandomInt(data.moves.length)].move.name}</p>
-                    <p class="moves_item">${data.moves[getRandomInt(data.moves.length)].move.name}</p>
-                </div>
+                ${renderContainerMovements(data.moves)}
             </div>
             <div>
                 <h4 class="information_title">Stats</h4>
-                <div class="container_stats">
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[0].base_stat}</p>
-                        <p class="item_value">${data.stats[0].stat.name}</p>
-                    </div>
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[1].base_stat}</p>
-                        <p class="item_value">${data.stats[1].stat.name}</p>
-                    </div>
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[2].base_stat}</p>
-                        <p class="item_value">${data.stats[2].stat.name}</p>
-                    </div>
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[3].base_stat}</p>
-                        <p class="item_value">${data.stats[3].stat.name}</p>
-                    </div>
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[4].base_stat}</p>
-                        <p class="item_value">${data.stats[4].stat.name}</p>
-                    </div>
-                    <div class="container_item">
-                        <p class="item_value">${data.stats[5].base_stat}</p>
-                        <p class="item_value">${data.stats[5].stat.name}</p>
-                    </div>
-                </div>
+                ${renderContainerStats(data.stats)}
             </div>
         </div>
         
@@ -66,10 +35,31 @@ function renderPokeCardData(data){
     containerPokemons.appendChild(cardPokemon);
 }
 
-function rederMovesPokemon(moves){
-    
-
+function renderContainerMovements(moves){
+    let containerMoves = document.createElement("div");
+    containerMoves.className = "container_moves";
+    for (let i = 0; i < 4; i++){
+        containerMoves.innerHTML+=`
+            <p class="moves_item">${moves[getRandomInt(moves.length)].move.name}</p>
+        `
+    }
+    return containerMoves.outerHTML;
 }
+
+function renderContainerStats(stats){
+    let containerStats = document.createElement("div");
+    containerStats.className = "container_stats";
+    for (let i = 0; i<stats.length; i++){
+        containerStats.innerHTML += `
+            <div class="container_item">
+                <p class="item_value">${stats[i].base_stat}</p>
+                <p class="item_value">${stats[i].stat.name}</p>
+            </div>
+        `
+    }
+    return containerStats.outerHTML;
+}
+
 
 let containerPokemons = document.getElementsByClassName("pokemon_container")[0]
 let buttonBuscar = document.getElementById("form_btnBuscar");
